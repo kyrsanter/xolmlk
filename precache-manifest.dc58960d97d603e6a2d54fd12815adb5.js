57c61fba38bba5d51f06b88a50a1d59a3323c754
Admin <you@example.com>
2019-11-14 14:25:00 +0300

    Updates

---
+++ precache-manifest.dc58960d97d603e6a2d54fd12815adb5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5e082098b57efd4b265e9b1f641cc0",
+    "url": "/xolmlk/index.html"
+  },
+  {
+    "revision": "8a9f3640a6247a8dbd10",
+    "url": "/xolmlk/static/css/main.e487b2ca.chunk.css"
+  },
+  {
+    "revision": "fcbcc1422b3b7b9edea6",
+    "url": "/xolmlk/static/js/2.9c9485f5.chunk.js"
+  },
+  {
+    "revision": "8a9f3640a6247a8dbd10",
+    "url": "/xolmlk/static/js/main.cd952b8e.chunk.js"
+  },
+  {
+    "revision": "fbc525d6ed2ec558d10a",
+    "url": "/xolmlk/static/js/runtime-main.f107631f.js"
+  }
+]);
